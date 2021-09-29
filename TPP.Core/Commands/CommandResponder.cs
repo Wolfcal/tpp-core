b018bdd4dbe7833e37f3481e52772b7d255aa881
felk <de.felix.koenig@gmail.com>
2021-09-29 19:21:13 +0200

    convert namespace declarations to C# 10 file-scoped namespaces

--- TPP.Core/Commands/CommandResponder.cs
+++ TPP.Core/Commands/CommandResponder.cs
@@ -2,67 +2,66 @@ using System;
 using System.Threading.Tasks;
 using TPP.Core.Chat;
 
-namespace TPP.Core.Commands
+namespace TPP.Core.Commands;
+
+public interface ICommandResponder
 {
-    public interface ICommandResponder
+    public Task ProcessResponse(Message message, CommandResult result);
+}
+
+public class CommandResponder : ICommandResponder
+{
+    private readonly IMessageSender _messageSender;
+    private readonly int _whisperIfLongThreshold;
+
+    public CommandResponder(IMessageSender messageSender, int whisperIfLongThreshold = 100)
     {
-        public Task ProcessResponse(Message message, CommandResult result);
+        _messageSender = messageSender;
+        _whisperIfLongThreshold = whisperIfLongThreshold;
     }
 
-    public class CommandResponder : ICommandResponder
+    public async Task ProcessResponse(Message message, CommandResult result)
     {
-        private readonly IMessageSender _messageSender;
-        private readonly int _whisperIfLongThreshold;
-
-        public CommandResponder(IMessageSender messageSender, int whisperIfLongThreshold = 100)
+        if (result.Response == null) return;
+        switch (result.ResponseTarget)
         {
-            _messageSender = messageSender;
-            _whisperIfLongThreshold = whisperIfLongThreshold;
-        }
-
-        public async Task ProcessResponse(Message message, CommandResult result)
-        {
-            if (result.Response == null) return;
-            switch (result.ResponseTarget)
-            {
-                case ResponseTarget.Source:
-                    if (message.MessageSource == MessageSource.Chat)
-                        await _messageSender.SendMessage($"@{message.User.Name} {result.Response}");
-                    else if (message.MessageSource == MessageSource.Whisper)
-                        await _messageSender.SendWhisper(message.User, result.Response);
-                    break;
-                case ResponseTarget.Chat:
+            case ResponseTarget.Source:
+                if (message.MessageSource == MessageSource.Chat)
                     await _messageSender.SendMessage($"@{message.User.Name} {result.Response}");
-                    break;
-                case ResponseTarget.Whisper:
+                else if (message.MessageSource == MessageSource.Whisper)
                     await _messageSender.SendWhisper(message.User, result.Response);
-                    break;
-                case ResponseTarget.WhisperIfLong:
-                    if (message.MessageSource == MessageSource.Chat)
+                break;
+            case ResponseTarget.Chat:
+                await _messageSender.SendMessage($"@{message.User.Name} {result.Response}");
+                break;
+            case ResponseTarget.Whisper:
+                await _messageSender.SendWhisper(message.User, result.Response);
+                break;
+            case ResponseTarget.WhisperIfLong:
+                if (message.MessageSource == MessageSource.Chat)
+                {
+                    if (result.Response.Length > _whisperIfLongThreshold)
                     {
-                        if (result.Response.Length > _whisperIfLongThreshold)
-                        {
-                            await _messageSender.SendMessage(
-                                $"@{message.User.Name} The reply has been whispered to you.");
-                            await _messageSender.SendWhisper(message.User, result.Response);
-                        }
-                        else
-                        {
-                            await _messageSender.SendMessage($"@{message.User.Name} {result.Response}");
-                        }
+                        await _messageSender.SendMessage(
+                            $"@{message.User.Name} The reply has been whispered to you.");
+                        await _messageSender.SendWhisper(message.User, result.Response);
                     }
                     else
                     {
-                        await _messageSender.SendWhisper(message.User, result.Response);
+                        await _messageSender.SendMessage($"@{message.User.Name} {result.Response}");
                     }
-                    break;
-                case ResponseTarget.NoneIfChat:
-                    if (message.MessageSource != MessageSource.Chat)
-                        await _messageSender.SendWhisper(message.User, result.Response);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(result.ResponseTarget));
-            }
+                }
+                else
+                {
+                    await _messageSender.SendWhisper(message.User, result.Response);
+                }
+                break;
+            case ResponseTarget.NoneIfChat:
+                if (message.MessageSource != MessageSource.Chat)
+                    await _messageSender.SendWhisper(message.User, result.Response);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(result.ResponseTarget));
         }
     }
 }
